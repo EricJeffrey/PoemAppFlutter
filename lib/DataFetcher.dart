7d24bf15c59856b0b92e4f1f6b156e2bf9e24e6e
EricJeffrey <1719937412@qq.com>
2019-02-06 23:12:34 +0800

    implement http request, trying json transfer

--- lib/DataFetcher.dart
+++ lib/DataFetcher.dart
@@ -1,15 +1,51 @@
-import 'package:dio/dio.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
 class Poem {
   String title, author, authorDynasty;
   List<String> lines;
   Poem({this.title, this.author, this.authorDynasty, this.lines});
+
+  factory Poem.fromJson(Map<String, dynamic> json) {
+    Poem res = Poem(
+      title: json['title'],
+      author: json['author'],
+      authorDynasty: json['authorDynasty'],
+      lines: [],
+    );
+    for (var item in json['lines']) res.lines.add(item);
+    return res;
+  }
+
+  bool isPoemValid() {
+    if (title == null ||
+        author == null ||
+        authorDynasty == null ||
+        lines == null) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    const sp = "---";
+    String res = title + sp + author + sp + authorDynasty + sp;
+    for (var item in lines) res += item + sp;
+    return res;
+  }
 }
 
-class DataFetcher {
-  void getToday() async {
-    Response response;
-    Dio dio = new Dio();
-    response = await dio.get("localhost:8000/");
-    return print(response);
+Future<Poem> fetchData() async {
+  final Response response = await get("http://192.168.137.1:8000");
+  if (response.statusCode == 200) {
+    Map<String, dynamic> tmp =
+        jsonDecode(Utf8Decoder().convert(response.bodyBytes));
+    return Poem.fromJson(tmp);
+  } else {
+    throw Exception("failed to load data");
   }
+}
+
+void mylog(Object s, {String desc}) {
+  if (desc == null) desc = "";
+  print("\n$desc:<<<<<<<<<<<<<<${s.toString()}>>>>>>>>>>>>>>\n");
 }
